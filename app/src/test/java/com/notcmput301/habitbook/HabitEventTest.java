4ad184522e6ca28899de35f6e2e37fbb9731c473
Yi Zhang <yi16@ualberta.ca>
2017-10-21 14:23:32 -0600

    habit event test

--- app/src/test/java/com/notcmput301/habitbook/HabitEventTest.java
+++ app/src/test/java/com/notcmput301/habitbook/HabitEventTest.java
@@ -4,10 +4,25 @@ package com.notcmput301.habitbook;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class HabitEventTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void createEvent(){
+        Date date = new Date();
+        ArrayList<String> weekday = new ArrayList<>();
+        weekday.add("Monday");
+        User user = new User("user", "password");
+        HabitType habit = new HabitType(user, "habit1", "test", date, weekday);
+        HabitEvent event = new HabitEvent(habit, "comment");
     }
 }
