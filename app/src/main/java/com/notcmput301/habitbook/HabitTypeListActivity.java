5f8387d8e08d9e15c6b57c4f3c149d60982d75f4
Yi Zhang <yizhang@Yis-MacBook-Pro.local>
2017-11-05 13:10:31 -0700

    UI

--- app/src/main/java/com/notcmput301/habitbook/HabitTypeListActivity.java
+++ app/src/main/java/com/notcmput301/habitbook/HabitTypeListActivity.java
@@ -1,7 +1,9 @@
 package com.notcmput301.habitbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -17,5 +19,14 @@ public class HabitTypeListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_type_list);
+
+        Button NewHabit = (Button) findViewById(R.id.NewHabit);
+        NewHabit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HabitTypeListActivity.this, CreateHabitActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
