c43811c7cea8c6914f984dd2528b30bba53c8ae8
Yi Zhang <yi16@ualberta.ca>
2017-11-06 23:26:59 -0700

    30% done of habit type detail

--- app/src/main/java/com/notcmput301/habitbook/HabitTypeDetailsActivity.java
+++ app/src/main/java/com/notcmput301/habitbook/HabitTypeDetailsActivity.java
@@ -2,37 +2,62 @@ package com.notcmput301.habitbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 public class HabitTypeDetailsActivity extends AppCompatActivity {
     private User LoggedInUser;
     private HabitType habit;
-    private TextView titleView;
-    private TextView reasonView;
     private EditText titleText;
     private EditText reasonText;
     private TextView habitSchedule;
     private TextView weekdays;
-    private ToggleButton mondayButton;
-    private ToggleButton tuesdayButton;
-    private ToggleButton wednesdayButton;
-    private ToggleButton thursdayButton;
-    private ToggleButton fridayButton;
-    private ToggleButton saturdayButton;
-    private ToggleButton sundayButton;
+    private CheckBox monday;
+    private CheckBox tuesday;
+    private CheckBox wednesday;
+    private CheckBox thursday;
+    private CheckBox friday;
+    private CheckBox saturday;
+    private CheckBox sunday;
     private TextView completionStatus;
     private Button back;
     private Button update;
     private Button delete;
     private Button addNewHabitEvent;
 
+    private String target;
+    private Gson gson;
+    private User user;
+    private ArrayList<HabitType> habitTypes;
+    private LocalFileControler localFileControler;
+    private HabitType currentHabitType;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_type_details);
+
+        Bundle bundle = getIntent().getExtras();
+        int index = bundle.getInt("index");
+        this.target = bundle.getString("user");
+        this.gson = new Gson();
+        this.user = gson.fromJson(target, User.class);
+        this.habitTypes = user.getHabitTypes();
+        this.currentHabitType = this.habitTypes.get(index);
+        this.target = bundle.getString("localfilecontroller");
+        this.localFileControler = gson.fromJson(target, LocalFileControler.class);
+
+        titleText = (EditText) findViewById(R.id.Title);
+        titleText.setText(currentHabitType.getTitle());
     }
 
     public void displayCompletionStatus(HabitType Habit){
