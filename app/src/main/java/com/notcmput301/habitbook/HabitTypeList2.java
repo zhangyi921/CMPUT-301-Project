61a9847f97458ca003ac4366ef03ee1c0e6034fe
Yi Zhang <yi16@ualberta.ca>
2017-11-22 15:57:34 -0700

    habittypelist

--- app/src/main/java/com/notcmput301/habitbook/HabitTypeList2.java
+++ app/src/main/java/com/notcmput301/habitbook/HabitTypeList2.java
@@ -13,21 +13,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-    private User loggedInUser;
-    private Gson gson = new Gson();
+import java.util.ArrayList;
 
+public class HabitTypeList2 extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private User loggedInUser;
+    private ArrayList<HabitType> habitTypes;
+    private ArrayAdapter<HabitType> Adapter;
+    private Gson gson = new Gson();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_habit_type_list2);
+
         Intent receiver = getIntent();
         String u = receiver.getExtras().getString("passedUser");
-        this.loggedInUser = gson.fromJson(u, User.class);
+        loggedInUser = gson.fromJson(u, User.class);
+        fillList();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -63,7 +76,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.habit_type_list2, menu);
         return true;
     }
 
@@ -90,9 +103,6 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.habit_type) {
             // Handle the camera action
-            Intent habitType = new Intent(MainActivity.this, HabitTypeListActivity.class);
-            habitType.putExtra("passedUser", gson.toJson(loggedInUser));
-            startActivity(habitType);
         } else if (id == R.id.today_habit) {
 
         } else if (id == R.id.habit_event_history) {
@@ -102,7 +112,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.setting) {
 
         } else if (id == R.id.logout) {
-            Intent createAccount = new Intent(MainActivity.this, LoginActivity.class);
+            Intent createAccount = new Intent(HabitTypeList2.this, LoginActivity.class);
             startActivity(createAccount);
 
         }
@@ -110,5 +120,86 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void fillList(){
+        ListView habitlist = (ListView) findViewById(R.id.HabitList);
+        ElasticSearch.getHabitTypeList ghtl = new ElasticSearch.getHabitTypeList();
+        ghtl.execute(loggedInUser.getUsername());
+        try {
+            habitTypes = ghtl.get();
+            if (habitTypes==null){
+                habitTypes = new ArrayList<>();
+            }
+            loggedInUser.setHabitTypes(habitTypes);       //causes program to crash
+        }catch(Exception e){
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to retrieve items. Check connection", Toast.LENGTH_SHORT).show();
+        }
+
+        HabitTypeList2.HabitTypeAdapter hAdapter = new HabitTypeList2.HabitTypeAdapter();
+        habitlist.setAdapter(hAdapter);
+
+        habitlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent habitdetail = new Intent(HabitTypeList2.this, HabitTypeDetailsActivity.class);
+                habitdetail.putExtra("passedUser", gson.toJson(loggedInUser));
+                habitdetail.putExtra("passedHabitType", gson.toJson(habitTypes.get(position)));
+                startActivity(habitdetail);
+            }
+        });
+    }
+
+
+    public void HTLnewHabitType(View view){
+
+        Intent createHabit = new Intent(HabitTypeList2.this, CreateHabitActivity.class);
+        createHabit.putExtra("passedUser", gson.toJson(loggedInUser));
+        startActivity(createHabit);
+    }
+
+
+    public void HTLRefresh(View view){
+
+        fillList();
+    }
+
+
+    public void HTLBack(View view){
+
+        Intent mainmenu = new Intent(HabitTypeList2.this, MainMenuActivity.class);
+        mainmenu.putExtra("passedUser", gson.toJson(loggedInUser));
+        startActivity(mainmenu);
+    }
+
+
+    class HabitTypeAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return habitTypes.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.habit_type_list_layout, null);
+            TextView titleL = (TextView) convertView.findViewById(R.id.HTLIST_Title);
+            TextView descriptionL = (TextView) convertView.findViewById(R.id.HTLIST_Description);
+
+            titleL.setText(habitTypes.get(position).getTitle());
+            descriptionL.setText(habitTypes.get(position).getReason());
+            return convertView;
+        }
     }
 }
