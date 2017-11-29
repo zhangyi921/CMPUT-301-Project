25265ff3aa5df52569014b9df682598dbf6c10b0
Yi Zhang <yi16@ualberta.ca>
2017-11-28 22:29:34 -0700

    .

--- app/src/main/java/com/notcmput301/habitbook/HabitEventHistory2.java
+++ app/src/main/java/com/notcmput301/habitbook/HabitEventHistory2.java
@@ -52,12 +52,11 @@ public class HabitEventHistory2 extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.refresh);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                FillList();
             }
         });
 
