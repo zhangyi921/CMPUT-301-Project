e0e0f82558f1931aad4401f9872d385b594fb3f2
= <cole.miller800@yahoo.ca>
2017-12-01 21:32:51 -0700

    Today's Habits List completed

--- app/src/main/java/com/notcmput301/habitbook/MainActivity.java
+++ app/src/main/java/com/notcmput301/habitbook/MainActivity.java
@@ -13,13 +13,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private User loggedInUser;
+    private ArrayList<HabitType> habitTypes;
+    private ArrayAdapter<HabitType> Adapter;
     private Gson gson = new Gson();
 
     @Override
@@ -29,6 +41,7 @@ public class MainActivity extends AppCompatActivity
         Intent receiver = getIntent();
         String u = receiver.getExtras().getString("passedUser");
         this.loggedInUser = gson.fromJson(u, User.class);
+        fillList();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -36,8 +49,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Refreshed", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                fillList();
             }
         });
 
@@ -124,4 +137,116 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void fillList(){
+        ListView habitlist = (ListView) findViewById(R.id.TodayHabitList);
+        ElasticSearch.getHabitTypeList ghtl = new ElasticSearch.getHabitTypeList();
+        ghtl.execute(loggedInUser.getUsername());
+        try {
+            habitTypes = ghtl.get();
+            if (habitTypes==null){
+                habitTypes = new ArrayList<>();
+            }
+
+            Calendar calendar = Calendar.getInstance();
+            int day = calendar.get(Calendar.DAY_OF_WEEK);
+            Date currentDate = new Date();
+
+            for(int i = 0; i<habitTypes.size(); i++){
+                HabitType habit = habitTypes.get(i);
+                ArrayList<Boolean> days = habit.getWeekdays();
+                Date startDate = habit.getStartDate();
+                if(!days.get(day-1)){
+                    habitTypes.remove(habit);
+                    i--;
+                }
+                else if(startDate.after(currentDate)){
+                    habitTypes.remove(habit);
+                    i--;
+                }
+                else {
+                    ArrayList<HabitEvent> events = habit.getEvents();
+                    for(int j = 0; j < events.size(); j++) {
+                        HabitEvent event = events.get(j);
+                        Date eventDate = event.getDate();
+                        Calendar cal1 = Calendar.getInstance();
+                        Calendar cal2 = Calendar.getInstance();
+                        cal1.setTime(currentDate);
+                        cal2.setTime(eventDate);
+                        if (cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR)) {
+                            habitTypes.remove(habit);
+                            i--;
+                        }
+                    }
+                }
+            }
+            //loggedInUser.setHabitTypes(habitTypes);       //causes program to crash
+        }catch(Exception e){
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to retrieve items. Check connection", Toast.LENGTH_SHORT).show();
+        }
+        
+        MainActivity.HabitTypeAdapter hAdapter = new MainActivity.HabitTypeAdapter();
+        habitlist.setAdapter(hAdapter);
+
+        habitlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent habitdetail = new Intent(MainActivity.this, HabitTypeDetailsActivity.class);
+                habitdetail.putExtra("passedUser", gson.toJson(loggedInUser));
+                habitdetail.putExtra("passedHabitType", gson.toJson(habitTypes.get(position)));
+                startActivity(habitdetail);
+            }
+        });
+    }
+
+
+    /*public void HTLnewHabitType(View view){
+
+        Intent createHabit = new Intent(HabitTypeList2.this, CreateHabitActivity.class);
+        createHabit.putExtra("passedUser", gson.toJson(loggedInUser));
+        startActivity(createHabit);
+    }*/
+
+
+    public void HTLRefresh(View view){
+
+        fillList();
+    }
+
+
+
+
+    class HabitTypeAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return habitTypes.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.habit_type_list_layout, null);
+            TextView titleL = (TextView) convertView.findViewById(R.id.HTLIST_Title);
+            TextView descriptionL = (TextView) convertView.findViewById(R.id.HTLIST_Description);
+            TextView NumEvents = (TextView) convertView.findViewById(R.id.NumEvents);
+
+            NumEvents.setText("Number of total events: "+habitTypes.get(position).getTotalEvents().toString());
+            titleL.setText(habitTypes.get(position).getTitle());
+            descriptionL.setText(habitTypes.get(position).getReason());
+            return convertView;
+        }
+    }
+
 }
