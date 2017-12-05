a70e315286c76c63583e4ea958939f255a992355
Yi Zhang <yi16@ualberta.ca>
2017-12-04 21:23:25 -0700

    update map in habit event detail

--- app/src/main/java/com/notcmput301/habitbook/HabitEventDetailsActivity.java
+++ app/src/main/java/com/notcmput301/habitbook/HabitEventDetailsActivity.java
@@ -11,12 +11,20 @@
  */
 package com.notcmput301.habitbook;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,6 +38,8 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static com.notcmput301.habitbook.R.id.location;
 
 /**
  * Activity for displaying HabitEvent Details
@@ -46,10 +56,14 @@ public class HabitEventDetailsActivity extends AppCompatActivity {
     private ArrayList<HabitType> habitTypes;
     private NetworkHandler nH;
 
+    private LocationManager locationManager;
+    private LocationListener listener;
+    private Location currentlocation = null;
     private EditText titleE;
     private EditText commentE;
     private Button addPicB;
     private Button backB;
+    private Button location;
     private CircleImageView imageV;
     private Gson gson = new Gson();
 
@@ -86,6 +100,62 @@ public class HabitEventDetailsActivity extends AppCompatActivity {
         BroadcastReceiver br = new NetworkStateChangeReceiver();
         IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
         this.registerReceiver(br, filter);
+        location = (Button) findViewById(R.id.GetLocation);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Double la = location.getLatitude();
+                Double lo = location.getLongitude();
+                String s = la.toString()+"  "+lo.toString();
+                currentlocation = location;
+                Toast.makeText(HabitEventDetailsActivity.this, s, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        //Checks if Network Connection is detected.
+
+        configure_button();
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 500, 0, listener);
+            }
+        });
     }
 
     /**
@@ -120,6 +190,10 @@ public class HabitEventDetailsActivity extends AppCompatActivity {
                         nH.deleteHabitType(h);
                         h.removeHabitEvent(eve);
                         habitEvent.setComment(comment);
+                        if (currentlocation != null) {
+                            habitEvent.setLatitude(currentlocation.getLatitude());
+                            habitEvent.setLongitude(currentlocation.getLongitude());
+                        }
                         h.addHabitEvent(habitEvent);
                         nH.addHabitType(h);
                     }else{
